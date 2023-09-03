b1b3a9fd24af5ba37aee2d955d07b3be35a29881
roberthRenner <133983214+Roberth-Renner@users.noreply.github.com>
2023-09-03 20:01:24 -0300

    Update Manipulando dados Oficina.sql
    Apenas realizando a indentação.

--- Manipulando dados Oficina.sql
+++ Manipulando dados Oficina.sql
@@ -4,7 +4,7 @@
 -- QUANTAS EQUIPES E QUANTOS FUNCIONÁRIOS TRABALHAM EM CADA UMA DELAS --
 
 select 	EspecialidadeEquipe, idEquipe as Numero_Equipe, count(*) as Funcionarios_Na_Equipe
-		from Equipe
+	from Equipe
         join Mecanicos using(idEquipe)
         group by idEquipe;
         
@@ -16,7 +16,7 @@ select concat(FNome,' ',LNome) as Nome_Completo, Especialidade, Endereco, idEqui
 -- MOVIMENTAÇÃO DE PEÇAS --
 
 select concat(FNome, ' ', LNome) as Nome_Completo, OsStatus, NumeroOrdem, DataEmissao, Categoria, NomePeca, QuantidadePeca, ValorUnitárioPeca
-		from cliente c
+	from cliente c
         join OrdemServico os on os.idCliente = c.idCliente and OsStatus = 'Orçamento Aprovado'
         join Relacao_Os_Pecas using(idOrdemServico)
         join Pecas using(idPecas)
@@ -26,8 +26,8 @@ select concat(FNome, ' ', LNome) as Nome_Completo, OsStatus, NumeroOrdem, DataEm
 
 
 select (select concat(FNome, ' ', LNome) as Nome_Cliente from Cliente as c where c.idCliente = o.idCliente) as Nome_Cliente, 
-		OsStatus as Status_Aprovação, Categoria, NomePeca,Descricao as Servico, ValorUnitárioPeca, QuantidadePeca, ValorServico, ValorTotal
-		from Pecas 
+	OsStatus as Status_Aprovação, Categoria, NomePeca,Descricao as Servico, ValorUnitárioPeca, QuantidadePeca, ValorServico, ValorTotal
+	from Pecas 
         join Relacao_Os_Pecas as r using(idPecas)
         join OrdemServico as o on r.idOrdemServico = o.idOrdemServico and OsStatus = 'Orçamento Reprovado'
         join Relacao_Os_Servicos as ros on o.idOrdemServico = ros.idOrdemServico  
@@ -38,21 +38,21 @@ select (select concat(FNome, ' ', LNome) as Nome_Cliente from Cliente as c where
 -- QUANTIDADE DE OS QUE ULTRAPASSARAM OS 1000 REAIS --
 
 select concat(FNome, ' ', LNome) as Nome_Cliente, CPF, Contato, ValorTotal
-	   from Cliente as c, OrdemServico as os
+       from Cliente as c, OrdemServico as os
        where os.idCliente = c.idCliente and ValorTotal between 1000 and 20000
        order by ValorTotal asc; 
 
 -- MÉDIA DE DIAS GASTOS PARA MANUTENÇÃO, E PARA FORNECIMENTO DE PEÇAS --
 
 select  idEquipe, EspecialidadeEquipe, round(avg(datediff(DataConclusao, DataEmissao)),2) as MediaDedias
-		from OrdemServico
+	from OrdemServico
         join Equipe using(idEquipe)
         group by idEquipe;
 
 -- TIPO DE MANUTENÇÃO QUE TEM O PREÇO MAIS ROUBUSTO --
 
 select idEquipe as Numero_Equipe, EspecialidadeEquipe , round(avg(ValorTotal),2) as MediaDeValores
-		from Equipe
+	from Equipe
         join OrdemServico using(idEquipe)
         group by idEquipe;
 
@@ -60,6 +60,6 @@ select idEquipe as Numero_Equipe, EspecialidadeEquipe , round(avg(ValorTotal),2)
 --  TIPO DE MANUTENÇÃO QUE É MAIS REALIZADA --
 
 select TipoServiço, count(TipoServiço) as QuantidadeServicos
-		from Servicos s
+	from Servicos s
         group by TipoServiço;
         
